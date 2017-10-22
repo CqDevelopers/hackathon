ee3801d87f99c05e4fc1d5d520d607647b63798b
ShoheiMitani <user@ShoheiMitani.local>
2017-10-22 10:16:19 +0900

    UserListControllerを修正

--- src/main/java/hack/api/userList/UserListController.java
+++ src/main/java/hack/api/userList/UserListController.java
@@ -1,5 +1,6 @@
 package hack.api.userList;
 
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -10,6 +11,9 @@ import java.util.Map;
 import javax.inject.Inject;
 
 import org.apache.commons.beanutils.BeanUtils;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.dozer.Mapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
@@ -19,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import hack.domain.model.Total;
 import hack.domain.model.User;
 import hack.domain.service.user.UserService;
 
@@ -33,9 +38,10 @@ public class UserListController {
 	
   @RequestMapping(method = RequestMethod.GET)
   @ResponseStatus(HttpStatus.OK)
-  public List<UserListResource> getTodos() throws Exception{ 
+  public String getTodos() throws Exception{ 
 	  
-	//List<Map<String, Object>> createUser = new ArrayList<Map<String, Object>>();
+/*
+    //List<Map<String, Object>> createUser = new ArrayList<Map<String, Object>>();
 	Map<String, Object> map1 = new HashMap<>();
 	map1.put("id", "user01");
 	map1.put("userName", "クオリカ太郎");
@@ -70,6 +76,71 @@ public class UserListController {
   	  userListResponse.add(beanMapper.map(userinfo, UserListResource.class));
     }
     return userListResponse;
+    
+*/
+      
+      Map<String, Object> valueMap = new HashMap<>();
+      
+      List<User> users = new ArrayList<User>();
+      //1人目 ユーザ情報登録
+      User user = new User();
+      user.setId("user01");
+      user.setUserName("クオリカ太郎");
+      user.setLocation("東京都新宿区西新宿８丁目１７−１ 住友不動産新宿グランドタワー23F");
+      user.setPhotoUrl("http://localhost:3000/samples/image");
+      users.add(user);
+      
+      //2人目 ユーザ情報登録
+      user = new User();
+      user.setId("user02");
+      user.setUserName("クオリカ二郎");
+      user.setLocation("東京都新宿区西新宿８丁目１７−１ 住友不動産新宿グランドタワー23F");
+      user.setPhotoUrl("http://localhost:3000/samples/image");
+      users.add(user);
+      
+    //3人目 ユーザ情報登録
+      user = new User();
+      user.setId("user03");
+      user.setUserName("クオリカ三朗");
+      user.setLocation("東京都新宿区西新宿８丁目１７−１ 住友不動産新宿グランドタワー23F");
+      user.setPhotoUrl("http://localhost:3000/samples/image");
+      users.add(user);
+      
+      //「list」の内容を書きこむ
+      valueMap.put("list", users);
+      
+      
+      
+      //ユーザ数を格納
+      int total = 3;
+      Total userTotal = new Total();
+      userTotal.setTotal(total);
+      
+      //「resultInfo」に内容を書きこむ
+      valueMap.put("resultInfo", userTotal);
+      
+      
+      //JSON形式に変換するメソッドへ
+      return makeJSONString(valueMap);
+  }
+  
+  private static String makeJSONString(Map<String,Object> valueMap){
+      
+      ObjectMapper objectMapper = new ObjectMapper();
+      String jsonString = null;
+      
+      try {
+          jsonString = objectMapper.writeValueAsString(valueMap);
+      } catch (JsonGenerationException e) {
+          e.printStackTrace();
+      } catch (JsonMappingException e) {
+          e.printStackTrace();
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+      
+//      System.out.println(jsonString);
+      return jsonString;
   }
 
 }
---
+++ src/main/java/hack/domain/model/Total.java
@@ -0,0 +1,20 @@
+package hack.domain.model;
+
+import java.io.Serializable;
+
+public class Total implements Serializable{
+
+    
+    private static final long serialVersionUID = 1L;
+    
+    private int total;
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+}
