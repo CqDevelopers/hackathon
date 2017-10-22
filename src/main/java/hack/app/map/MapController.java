fe5ddbc40a2f6e3640522deab8ad343f4f3a8c90
NaoyaIkeda <naoya.ikeda.4001@gmail.com>
2017-10-22 13:25:27 +0900

    マップのURL変更

--- src/main/java/hack/app/map/MapController.java
+++ src/main/java/hack/app/map/MapController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("map1")
+@RequestMapping("map")
 public class MapController {
 	
-	@RequestMapping(value = "/")
+	@RequestMapping(value = "/1")
 	public String list() {
 		
         return "map/map1";
--- src/main/java/hack/app/map/MapController2.java
+++ src/main/java/hack/app/map/MapController2.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("map2")
+@RequestMapping("map")
 public class MapController2 {
 	
-	@RequestMapping(value = "/")
+	@RequestMapping(value = "/2")
 	public String list() {
 		
         return "map/map2";
